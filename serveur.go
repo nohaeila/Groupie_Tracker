c8baa846e6c49a2847b6506bf0a05cfce447ffdf
LouisLchipse <louis.luizet@ynov.com>
2024-02-27 14:43:19 +0100

    added infoHandler, info.html

--- serveur.go
+++ serveur.go
@@ -1,62 +1,84 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "html/template"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
 )
 
 // Structure pour décoder les données JSON de l'API
 type Artist struct {
-    ID        int    `json:"id"`
-    Name      string `json:"name"`
-    Date    int `json:"creationDate"`
-    Image     string `json:"image"`
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Date  int    `json:"creationDate"`
+	Image string `json:"image"`
+}
+
+type Relation struct {
 }
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-    http.HandleFunc("/", indexHandler)
-    http.HandleFunc("/liste", listeHandler) // Gestionnaire pour la page de liste
-    fmt.Println("Serveur démarré sur le port 8080")
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/liste", listeHandler) // Gestionnaire pour la page de liste
+	http.HandleFunc("/info", infoHandler)
+	fmt.Println("Serveur démarré sur le port 8080")
+	http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    err := templates.ExecuteTemplate(w, "accueil.html", nil)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	err := templates.ExecuteTemplate(w, "accueil.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func listeHandler(w http.ResponseWriter, r *http.Request) {
-    artists, err := getArtists()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	artists, err := getArtists()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    err = templates.ExecuteTemplate(w, "liste.html", artists)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	err = templates.ExecuteTemplate(w, "liste.html", artists)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	artists, err := getArtists()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = templates.ExecuteTemplate(w, "info.html", artists)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func getArtists() ([]Artist, error) {
-    resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-    var artists []Artist
-    err = json.NewDecoder(resp.Body).Decode(&artists)
-    if err != nil {
-        return nil, err
-    }
-    return artists, nil
+	var artists []Artist
+	err = json.NewDecoder(resp.Body).Decode(&artists)
+	if err != nil {
+		return nil, err
+	}
+	return artists, nil
+}
+
+func getRelations() {
+
 }
